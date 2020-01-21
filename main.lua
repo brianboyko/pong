4c873a7a5631d07c33b0f3e06393601bf44f1ce5
brianboyko <brian.boyko@gmail.com>
2020-01-21 20:38:29 +0000

    class and push are external libraries, while main.lua sets up window

--- main.lua
+++ main.lua
@@ -1,47 +1,41 @@
---[[
-    GD50 2018
-    Pong Remake
-
-    pong-0
-    "The Day-0 Update"
-
-    -- Main Program --
-
-    Author: Colton Ogden
-    cogden@cs50.harvard.edu
-
-    Originally programmed by Atari in 1972. Features two
-    paddles, controlled by players, with the goal of getting
-    the ball past your opponent's edge. First to 10 points wins.
-
-    This version is built to more closely resemble the NES than
-    the original Pong machines or the Atari 2600 in terms of
-    resolution, though in widescreen (16:9) so it looks nicer on 
-    modern systems.
-]]
+-- [[ Requirements: 
+--    Push is a library that allows us to draw our game at a virtual resolution
+-- ]]
+push = require 'push'
 
 WINDOW_WIDTH = 1280
 WINDOW_HEIGHT = 720
 
---[[
-    Runs when the game first starts up, only once; used to initialize the game.
-]]
+VIRTUAL_WIDTH = 432
+VIRTUAL_HEIGHT = 243
+
 function love.load()
-    love.window.setMode(WINDOW_WIDTH, WINDOW_HEIGHT, {
+    -- use nearest-neighbor filtering
+    love.graphics.setDefaultFilter('nearest', 'nearest')
+    -- setup push
+    push:setupScreen(VIRTUAL_WIDTH, VIRTUAL_HEIGHT, WINDOW_WIDTH, WINDOW_HEIGHT, {
         fullscreen = false,
         resizable = false,
         vsync = true
     })
 end
 
---[[
-    Called after update by LÖVE2D, used to draw anything to the screen, updated or otherwise.
-]]
+function love.keypressed(key)
+    if key == 'escape' then 
+        love.event.quit()
+    end
+end
+
 function love.draw()
+    -- begin rendering at virtual resolution
+    push:apply('start')
+    -- print
     love.graphics.printf(
         'Hello Pong!',          -- text to render
         0,                      -- starting X (0 since we're going to center it based on width)
-        WINDOW_HEIGHT / 2 - 6,  -- starting Y (halfway down the screen)
-        WINDOW_WIDTH,           -- number of pixels to center within (the entire screen here)
+        VIRTUAL_HEIGHT / 2 - 6,  -- starting Y (halfway down the screen)
+        VIRTUAL_WIDTH,           -- number of pixels to center within (the entire screen here)
         'center')               -- alignment mode, can be 'center', 'left', or 'right'
-end
+    -- end rendering at virtual resolution
+    push:apply('end')
+ end
