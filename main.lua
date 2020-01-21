8ed48d28de2cbb1412e563e4e20bb9b77d8fa1c9
brianboyko <brian.boyko@gmail.com>
2020-01-21 20:51:25 +0000

    Add rectangle drawing

--- main.lua
+++ main.lua
@@ -12,6 +12,11 @@ VIRTUAL_HEIGHT = 243
 function love.load()
     -- use nearest-neighbor filtering
     love.graphics.setDefaultFilter('nearest', 'nearest')
+
+    -- setup small font
+    smallFont = love.graphics.newFont('font.ttf', 8)
+    love.graphics.setFont(smallFont)
+
     -- setup push
     push:setupScreen(VIRTUAL_WIDTH, VIRTUAL_HEIGHT, WINDOW_WIDTH, WINDOW_HEIGHT, {
         fullscreen = false,
@@ -29,13 +34,18 @@ end
 function love.draw()
     -- begin rendering at virtual resolution
     push:apply('start')
+
+    -- clear the background with pong-greyish
+    love.graphics.clear(40/255, 45/255, 52/255, 1)
+
+    -- render first paddle
+    love.graphics.rectangle('fill', 10, 30, 5, 20)
+    -- render second paddle
+    love.graphics.rectangle('fill', VIRTUAL_WIDTH - 10, VIRTUAL_HEIGHT - 50, 5, 20)
+    -- render ball
+    love.graphics.rectangle('fill', VIRTUAL_WIDTH / 2 - 2, VIRTUAL_HEIGHT / 2 - 2, 4, 4)
     -- print
-    love.graphics.printf(
-        'Hello Pong!',          -- text to render
-        0,                      -- starting X (0 since we're going to center it based on width)
-        VIRTUAL_HEIGHT / 2 - 6,  -- starting Y (halfway down the screen)
-        VIRTUAL_WIDTH,           -- number of pixels to center within (the entire screen here)
-        'center')               -- alignment mode, can be 'center', 'left', or 'right'
+    love.graphics.printf( 'Hello Pong!', 0, 20, VIRTUAL_WIDTH, 'center')    
     -- end rendering at virtual resolution
     push:apply('end')
  end
